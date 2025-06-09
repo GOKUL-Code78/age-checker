68ac8e2a5af7456fb3fdf49e1510d68e9be685e4
GOKUL SANWAL <warriorsanwal@gmail.com>
2025-06-09 11:55:39 +0530

    error handling

--- Age_checker/Age_Checker.py
+++ Age_checker/Age_Checker.py
@@ -21,8 +21,7 @@ dob=st.date_input("Enter DOB",value=date(2000,1,1),min_value= date(1900,1,1),max
 if st.button("what's my age"):
     age=relativedelta(current_date,dob)
     st.success(f"{name} your age is {age.years} years {age.months} month and {age.days} days")
-years=age.years
-if years<18:
-    st.markdown("# oo you are under 18")
+if age.years>18:
+    st.info(f" {name} ✅ You are a adult")
 else:
-    st.markdown("** Oo means you are a adult**")
+    st.info(f"{name}you are a teenager ")
